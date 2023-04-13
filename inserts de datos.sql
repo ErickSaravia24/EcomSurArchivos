23d71bfff75e6abdbe991e7c28afd55736d2b820
ErickSaravia24 <erick_2408@hotmail.com>
2023-04-12 20:59:01 -0600

    se actualizan inserts de Roles y permisos

--- inserts de datos.sql
+++ inserts de datos.sql
@@ -1,17 +1,6 @@
 USe EcomSurUsuarios
-EXEC dbo.spInsertarUsuario
-    @fcNombre = 'Juan',
-    @fcAmaterno = 'Pérez',
-    @fcApaterno = 'González',
-    @fcCalle = 'Calle 123',
-    @fcNumero = '456',
-    @fcColonia = 'Colonia Centro',
-    @fcFechaCreacion = '2023-04-12 11:00:00',
-    @fcCorreo = 'juan.perez@example.com',
-    @fcPassword = 'mypassword',
-    @fiIdRol = 1;
 
-	INSERT INTO EcomSurUsuarios.dbo.BDRoles (fiName) VALUES ( 'Admin');
+INSERT INTO EcomSurUsuarios.dbo.BDRoles (fiName) VALUES ( 'Admin');
 INSERT INTO EcomSurUsuarios.dbo.BDRoles ( fiName) VALUES ( 'Worker');
 INSERT INTO EcomSurUsuarios.dbo.BDRoles ( fiName) VALUES ( 'Client');
 
@@ -19,38 +8,14 @@ INSERT INTO dbo.BDPermisos (fiName) VALUES ( 'Ver productos');
 INSERT INTO dbo.BDPermisos (fiName) VALUES ( 'Editar productos');
 INSERT INTO dbo.BDPermisos (fiName) VALUES ( 'Eliminar productos');
 INSERT INTO dbo.BDPermisos (fiName) VALUES ( 'Ver usuarios');
+INSERT INTO dbo.BDPermisos (fiName) VALUES ( 'Agregar productos');
+INSERT INTO dbo.BDPermisos (fiName) VALUES ( 'Agregar usuarios');
+INSERT INTO dbo.BDPermisos (fiName) VALUES ( 'Editar usuarios');
+INSERT INTO dbo.BDPermisos (fiName) VALUES ( 'eliminar usuarios');
 	
-
 INSERT INTO dbo.BDRolesPermisos(fiIdRol, fiIdPermiso)
-VALUES (1, 1), (1, 2), (1, 3), (1, 4), 
-	   (2, 1), (2, 2), (2, 3), 
+VALUES (1, 1), (1, 2), (1, 3), (1, 4),(1, 5), (1, 6),(1, 7),(1, 8),
+	   (2, 1), (2, 2), (2, 3), (2, 5),
 		(3, 1)
 
 
-		DELETE FROM dbo.BDPermisos
-		DELETE FROM dbo.BDRoles
-		DELETE FROM dbo.BDRolesPermisos
-		 DELETE FROM EcomSurUsuarios.dbo.BDUsuarios 
-		 DELETE FROM EcomSurProductos.dbo.BDUsuarios 
-		select * from dbo.BDPermisos
-		select * from dbo.BDRoles
-			select * from dbo.BDRolesPermisos
-
-			SELECT 
-    EU.fcNombre,
-    EU.fcApaterno,
-    EU.fcAmaterno,
-    EU.fcCalle,
-    EU.fcNumero,
-    EU.fcColonia,
-    EU.fcFechaCreacion,
-    EU.fcCorrero,
-	EU.fcPassword
-  
-FROM 
-    EcomSurUsuarios.dbo.BDUsuarios EU
-    INNER JOIN EcomSurProductos.dbo.BDUsuarios EP ON EU.fiUsuarioId = EP.fiUsuarioId
-
-
-	select * from EcomSurUsuarios.dbo.BDUsuarios
-	select * from EcomSurProductos.dbo.BDUsuarios
